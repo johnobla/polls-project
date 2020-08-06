bc2adbc873cca0337ff5b68852c36ca0d34cf031
John Obla <john-obla@live.co.uk>
2020-08-06 10:41:43 +0100

    Reduced code length/line in views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,13 +5,16 @@ from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    latest_question_list = Question.objects.order_by(
+        '-pub_date')[:5]
+    output = ', '.join([q.question_text
+                        for q in latest_question_list])
     return HttpResponse(output)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    return HttpResponse("You're looking at question %s."
+                        % question_id)
 
 
 def results(request, question_id):
@@ -20,4 +23,5 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse(
+        "You're voting on question %s." % question_id)
