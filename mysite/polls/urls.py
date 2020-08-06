2f8558caee5b019fccb103d8e6a91269a4320c18
John Obla <john-obla@live.co.uk>
2020-08-06 12:55:25 +0100

    Inherited template, removed hardcoded hrefs

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.index, name='index-polls'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
