082478f41e9ff5d6405d6ba4a9bad100736ec82d
John Obla <john-obla@live.co.uk>
2020-08-07 17:52:40 +0100

    Added more unit tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -17,3 +17,24 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=future_time)
 
         self.assertFalse(future_question.was_published_recently())
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        old_time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=old_time)
+
+        self.assertFalse(old_question.was_published_recently())
+
+    def test_was_published_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        recent_time = timezone.now() - datetime.timedelta(
+            hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=recent_time)
+
+        self.assertTrue(recent_question.was_published_recently())
